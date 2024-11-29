1fd965c77745f94252572c851877fc15543be58f
eedu7 <mueedahmad067@gmail.com>
2024-11-29 16:02:44 +0500

    FEAT: Added the useRegister and userLogin API, and their request and response type

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,1 +1,31 @@
-console.log(process.env.AUTH_URL)
+import axios from "axios";
+import {
+    LoginUserAPIRequestData, LoginUserAPIResponseData, RegisterUserAPIRequestData, RegisterUserAPIResponseData
+} from "@/api/auth/types";
+
+
+const authAPI = axios.create({
+    baseURL: "http://192.168.18.33:8001/v1/auth", headers: {
+        "Content-Type": "application/json",
+    }
+});
+
+
+export const userRegisterAPI = async (data: RegisterUserAPIRequestData): Promise<RegisterUserAPIResponseData> => {
+    try {
+        const response = await authAPI.post("/", data);
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Registration failed");
+    }
+}
+
+
+export const userLoginAPI = async (data: LoginUserAPIRequestData): Promise<LoginUserAPIResponseData> => {
+    try {
+        const response = await authAPI.post("/login", data);
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Login failed");
+    }
+}
---
+++ src/api/auth/types.ts
@@ -0,0 +1,18 @@
+export type RegisterUserAPIRequestData = {
+    username: string; emai: string; password: string;
+}
+
+export type LoginUserAPIRequestData = {
+    email: string; password: string;
+}
+
+export type RegisterUserAPIResponseData = {
+    message: string;
+}
+
+export type LoginUserAPIResponseData = {
+    access_token: string; refresh_token: string;
+}
+
+
+
